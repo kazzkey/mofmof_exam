d3602038469daef483f1da2a0e6f5641b8b690d9
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-20 15:57:32 +0900

    日本語辞書登録&不要ファイル削除

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
   has_many :near_stations, dependent: :destroy
   accepts_nested_attributes_for :near_stations, allow_destroy: true
+  validates :name,  presence: true
+  validates :rent,  presence: true
+  validates :address,  presence: true
+  validates :age,  presence: true
 end
